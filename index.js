d4a9ea351345d0822c9b4799a77933a2cf538849
Yatin Nagpal <nnega4428@gmail.com>
2022-11-04 19:36:51 +1100

    changes with time

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ window.setInterval(func, 1000);
 function func() {
   let d = new Date();
   let time = d.toLocaleTimeString()
-  time = time.includes('M')? time.slice(0,6): time
+  time = time.includes('M')? time.slice(0,7): time
   document.getElementById("time").innerHTML = time;
   document.getElementById("date").innerHTML = d.toLocaleDateString();
 }
