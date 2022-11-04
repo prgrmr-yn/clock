5d34f20a135ed7dc58361a565ebc0620512038d7
Yatin Nagpal <nnega4428@gmail.com>
2022-11-04 19:44:37 +1100

    deleting am or pm if appears

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ window.setInterval(func, 1000);
 function func() {
   let d = new Date();
   let time = d.toLocaleTimeString()
-  time = time.includes('M')? time.slice(0,7): time
+  time = time.includes('M')? time.replace(/AM/,''): time
   document.getElementById("time").innerHTML = time;
   document.getElementById("date").innerHTML = d.toLocaleDateString();
 }
