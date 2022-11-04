c12947cf21bc989f854dc41d0bd44dcff7e9e06b
Yatin Nagpal <nnega4428@gmail.com>
2022-11-04 20:20:37 +1100

    last

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ window.setInterval(func, 1000);
 function func() {
   let d = new Date().toString();
   let time = d.slice(16,24);
-  console.log(time);
+  console.log(d);
   document.getElementById("time").innerHTML = time;
-  document.getElementById("date").innerHTML = 'Little progress everyday';
+  document.getElementById("date").innerHTML = d.slice(0,16);
 }
